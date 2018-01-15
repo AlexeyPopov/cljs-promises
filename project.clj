0709978a0efc0cdfa0b9773db1066930369933fc
Alexey Popov <a.popov@zima.io>
2018-01-15 17:15:43 +0300

    fix resolve comiler warning

--- project.clj
+++ project.clj
@@ -1,26 +1,26 @@
-(defproject jamesmacaulay/cljs-promises "0.1.0"
-  :description "A ClojureScript library for working with JavaScript promises"
+(defproject alexeypopov/cljs-promises "0.1.0"
+  :description "A ClojureScript library for working with JavaScript promises. Fork with resolve warning fix"
 
-  :url "https://github.com/jamesmacaulay/cljs-promises"
+  :url "https://github.com/AlexeyPopov/cljs-promises"
 
   :scm {:name "git"
-        :url "https://github.com/jamesmacaulay/cljs-promises"}
+        :url "https://github.com/AlexeyPopov/cljs-promises"}
 
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojurescript "0.0-2156"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
   :source-paths ["src"]
 
   :cljsbuild {
-    :builds [{:id "examples"
-              :source-paths ["src" "examples/src"]
-              :compiler {
-                :output-to "examples/js/examples.js"
-                :output-dir "examples/out"
-                :optimizations :none
-                :source-map true}}]})
+              :builds [{:id "examples"
+                        :source-paths ["src" "examples/src"]
+                        :compiler {
+                                   :output-to "examples/js/examples.js"
+                                   :output-dir "examples/out"
+                                   :optimizations :none
+                                   :source-map true}}]})
